bc2147fad54cc1b6e0e869f728a2da1effb00676
Maksim Zhydovich <maksim.zhidovich@yandex.by>
2021-09-22 20:53:44 +0300

    fix gauss with main element

--- gauss_method_1.py
+++ gauss_method_1.py
@@ -5,13 +5,14 @@ def gauss_method(a, b):
 
     n = len(a)
     changed_a = a.copy()
+    swaps_x = [i for i in range(n)]
 
     for k in range(n):
         max_index = changed_a[k].index(max(changed_a[k], key= abs))
         # find max element in row and swap with current
         for c in range(n):
-            changed_a[c][max_index], changed_a[c][k] = \
-                changed_a[c][k], changed_a[c][max_index]
+            changed_a[c][max_index], changed_a[c][k] = changed_a[c][k], changed_a[c][max_index]
+        swaps_x[k], swaps_x[max_index] = swaps_x[max_index], swaps_x[k]
 
         for i in range(k + 1, n):
             factor = changed_a[i][k] / changed_a[k][k]
@@ -23,20 +24,17 @@ def gauss_method(a, b):
     for k in range(n - 1, -1, -1):
         sum_of_previous_x = 0
         for i in range(k + 1, n):
-            sum_of_previous_x += changed_a[k][i] * x[i]
-        x[k] = (1 / changed_a[k][k]) * (b[k] - sum_of_previous_x)
+            sum_of_previous_x += changed_a[k][i] * x[swaps_x[i]]
+        x[swaps_x[k]] = (1 / changed_a[k][k]) * (b[k] - sum_of_previous_x)
 
     return x
 
 
 def main():
     """
-    a = [[4, 5, 1, 2, 3],
-         [7, 6, 5, 4, 2],
-         [2, 8, 2, 3, 9],
-         [9, 1, 3, 5, 1],
-         [5, 3, 4, 1, 8]]
-    b = [9, 34, 30, 8, 15]
+    a = [[1, 2],
+         [2, 1]]
+    b = [-1, 1]
     """
 
     """
@@ -46,12 +44,21 @@ def main():
     b = [9, 5, 0]
     """
 
+    """
     a = [[1, -2, 3, -1],
          [2, 3, -4, 4],
          [3, 1, -2, -2],
          [1, -3, 7, 6]]
 
     b = [6, -7, 9, -7]
+    """
+
+    a = [[4, 5, 1, 2, 3],
+         [7, 6, 5, 4, 2],
+         [2, 8, 2, 3, 9],
+         [9, 1, 3, 5, 1],
+         [5, 3, 4, 1, 8]]
+    b = [9, 34, 30, 8, 15]
 
     solution = gauss_method(a, b)
     print(solution)
