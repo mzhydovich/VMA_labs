4eef7642441ac241863df3f4b8192b76a76a96c1
Maksim Zhydovich <maksim.zhidovich@yandex.by>
2021-09-28 23:52:33 +0300

    add lab_01.py

--- gauss_method.py
+++ gauss_method.py
@@ -1,9 +1,7 @@
 
 # Gauss method without main element
-
 def gauss_method(a, b):
     """Function return solution(x) of a system of linear algebraic equations ax = b"""
-
     n = len(a)
 
     for k in range(n):
@@ -19,6 +17,36 @@ def gauss_method(a, b):
         for i in range(k + 1, n):
             sum_of_previous_x += a[k][i] * x[i]
         x[k] = (1 / a[k][k]) * (b[k] - sum_of_previous_x)
+
+    return x
+
+
+# Gauss method with main element
+def gauss_method_with_main_element(a, b):
+
+    n = len(a)
+    changed_a = a.copy()
+    swaps_x = [i for i in range(n)]
+
+    for k in range(n):
+        max_index = changed_a[k].index(max(changed_a[k], key= abs))
+        # find max element in row and swap with current
+        for c in range(n):
+            changed_a[c][max_index], changed_a[c][k] = changed_a[c][k], changed_a[c][max_index]
+        swaps_x[k], swaps_x[max_index] = swaps_x[max_index], swaps_x[k]
+
+        for i in range(k + 1, n):
+            factor = changed_a[i][k] / changed_a[k][k]
+            for j in range(k, n):
+                changed_a[i][j] -= changed_a[k][j] * factor
+            b[i] -= b[k] * factor
+
+    x = [0] * n
+    for k in range(n - 1, -1, -1):
+        sum_of_previous_x = 0
+        for i in range(k + 1, n):
+            sum_of_previous_x += changed_a[k][i] * x[swaps_x[i]]
+        x[swaps_x[k]] = (1 / changed_a[k][k]) * (b[k] - sum_of_previous_x)
 
     return x
 
@@ -52,8 +80,11 @@ def main():
          [5, 3, 4, 1, 8]]
     b = [9, 34, 30, 8, 15]
 
-    solution = gauss_method(a, b)
-    print(solution)
+    solution1 = gauss_method(a, b)
+    solution2 = gauss_method_with_main_element(a, b)
+
+    print(f"Solution without main element: {solution1}")
+    print(f"Solution with main element: {solution2}")
 
 
 if __name__ == "__main__":
---
+++ lab_01.py
@@ -0,0 +1,92 @@
+from random import randint
+from gauss_method import gauss_method
+from gauss_method import gauss_method_with_main_element
+
+
+def multiply(a, b):
+    """Multiply matrix `a` by vector `b`"""
+    n = len(a)
+    m = len(b)
+
+    x = [0] * n
+    for i in range(n):
+        xi = 0
+        for q in range(m):
+            xi += a[i][q] * b[q]
+        x[i] = xi
+
+    return x
+
+
+def print_matrix(a):
+    """Print matrix"""
+    n = len(a)
+    m = len(a[0])
+    for i in range(n):
+        for q in range(m):
+            print(f"{a[i][q]:>10}", end= " ")
+        print()
+
+
+def generate_matrix(n, a, b):
+    """Generate matrix [n*n] of random numbers from `a` to `b`"""
+    matrix = [0] * n
+    for i in range(n):
+        row = [0] * n
+        for q in range(n):
+            row[q] = randint(a, b)
+        matrix[i] = row
+
+    return matrix
+
+
+def generate_vector(n, a, b):
+    """Generate vector [n] of random numbers from `a` to `b`"""
+    vector = [0] * n
+    for i in range(n):
+        vector[i] = randint(a, b)
+
+    return vector
+
+
+def get_norm_of_vector(x):
+    """Find the cubic norm of vector"""
+    return max([abs(el) for el in x])
+
+
+def subtract(a, b):
+    """Get subtraction b from a"""
+    return [(a[i] - b[i]) for i in range(len(a))]
+
+
+def main():
+
+    # generate matrix
+    A = generate_matrix(10, -1000000, 1000000)
+    # generate vector
+    x = generate_vector(10, -1000000, 1000000)
+
+    print("Matrix A: ")
+    print_matrix(A)
+    print(f"\nVector x: {x}")
+
+    # find `A * x`
+    f = multiply(A, x)
+    print(f"\nf = A * x = {f}")
+
+    # solve equation using gauss method
+    x1 = gauss_method(A, f)
+    print(f"\nx1* = {x1}")
+    # solve equation using gauss method with main element
+    x2 = gauss_method_with_main_element(A, f)
+    print(f"x2* = {x2}")
+
+    # find relative error
+    relative_error1 = get_norm_of_vector(subtract(x1, x)) / get_norm_of_vector(x)
+    print(f"\nRelative error for solution1: {relative_error1}")
+    relative_error2 = get_norm_of_vector(subtract(x2, x)) / get_norm_of_vector(x)
+    print(f"Relative error for solution2: {relative_error2}")
+
+
+if __name__ == "__main__":
+    main()
